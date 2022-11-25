3113c2908e702e5cf01f2f06c8ff2e9d9dff118b
lsantiago123 <lucas.santiago@unifesp.br>
2022-11-25 15:41:44 -0300

    'feat-crud'

---
+++ ReactAPI/Data/AdventureWorksContext.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ReactAPI.Data.Entities;
+
+namespace ReactAPI.Data
+{
+
+    public class AdventureWorksContext : DbContext
+    {
+        public AdventureWorksContext(DbContextOptions<AdventureWorksContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        // public DbSet<CustomerAddress> CustomerAddress { get; set; }
+        // public DbSet<ProductCategory> ProductCategory { get; set; }
+        // public DbSet<ProductDescription> ProductDescription { get; set; }
+        // public DbSet<ProductModel> ProductModel { get; set; }
+        // public DbSet<ProductModelProductDescription> ProductModelProductDescription { get; set; }
+        // public DbSet<SalesOrderDetail> SalesOrderDetail { get; set; }
+        // public DbSet<SalesOrderHeader> SalesOrderHeader { get; set; }
+
+
+    }
+}
---
+++ ReactAPI/Data/Entities/Customer.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactAPI.Data.Entities
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string? NameStyle { get; set; }
+        public string? Title { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? Sulfix { get; set; }
+        public string? CompanyName { get; set; }
+        public string? SalesPerson { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? Phone { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? PasswordSalt { get; set; }
+        public Guid rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
