add17dfcd9491c804114aab95edd312056fd3013
Higor-SM <higormariano10@gmail.com>
2022-12-01 18:24:03 -0300

    feat: delete function

--- crud-web/src/pages/AllAddresses.js
+++ crud-web/src/pages/AllAddresses.js
@@ -3,9 +3,12 @@ import { useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
 import { useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import DeleteConfirmation from "../components/DeleteConfirmation";
 
 function AllAddresses() {
   const [address, setAddress] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [itemToDelete, setItemToDelete] = useState(0);
 
   const navigate = useNavigate();
 
@@ -15,8 +18,38 @@ function AllAddresses() {
     });
   }, []);
 
+  function showConfirmPopupHandler(id){ //ele colocou id aqui mas não tenho certeza se é certo, 2:34:26
+    setShowModal(true);
+    setItemToDelete(id); //2:35:37
+  }
+
+  function closeConfirmPopupHandler(){
+    setShowModal(false);
+    setItemToDelete(0); //2:38:58
+  }
+
+  function deleteConfirmHandler(){
+    axios.delete(`https://localhost:7158/api/Addresses/${itemToDelete}`)
+    .then((response)=>{
+      setAddress((existingdata)=>{        //vou fazer q nem o dele, mas vai dar errado eu acho 2:37:51
+        return existingdata.filter(_ => _.id !== itemToDelete)
+      })
+      setItemToDelete(0);
+      setShowModal(false);
+    });
+  }
+
   return (
     <>
+    <DeleteConfirmation
+    showModal={showModal}
+    title = "Delete Confirmation!"
+    body = "Are sure to delete the item?"
+    closeConfirmPopupHandler = {closeConfirmPopupHandler}
+    deleteConfirmHandler = {deleteConfirmHandler}
+    >
+
+    </DeleteConfirmation>
       <div style={{ float: "right" }}>
         <Button
           variant="primary"
@@ -53,10 +86,20 @@ function AllAddresses() {
                   color: "black",
                 }}
                 variant="primary"
-                type="submit"
+                type="button"
                 onClick={() => {
                   navigate(`/updateAddress/${ad.addressID}`);
                 }}
+              >
+                Edit
+              </Button>
+              <Button
+                style={{
+                  color: "black",
+                }}
+                variant="danger"
+                type="button"
+                onClick={()=>{showConfirmPopupHandler(ad.id); }} //tbm não tenho certeza se está certo 2:35:17
               >
                 Edit
               </Button>
