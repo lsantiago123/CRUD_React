475b89ad752c24a8e25748effced6ff5b564663b
lsantiago123 <lucas.santiago@unifesp.br>
2022-12-21 10:59:38 -0300

    feat: API in Azure

--- ReactAPI/Program.cs
+++ ReactAPI/Program.cs
@@ -35,11 +35,11 @@ builder.Services.AddDbContext<AdventureWorksContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
